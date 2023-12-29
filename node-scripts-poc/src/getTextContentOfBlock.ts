cfaf720183dd8ccedcd69ceff20f5ad24fec20a2
mbezard <matthieube@bam.tech>
2023-12-29 15:44:46 +0100

    feat: implement function to read notion page

---
+++ node-scripts-poc/src/getTextContentOfBlock.ts
@@ -0,0 +1,63 @@
+import { Client } from "@notionhq/client";
+import { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+export const getTextContentOfBlockFromId = async (
+  blockId: string,
+  notion: Client
+): Promise<string> => {
+  const blocks = await notion.blocks.children.list({
+    block_id: blockId,
+  });
+
+  const content = await Promise.all(
+    blocks.results.map(async (block) => {
+      if (!("type" in block)) return;
+      const text = getTextContentOfBlockWithoutChildren(block);
+      if (!block.has_children) return text;
+
+      const childrenText = await getTextContentOfBlockFromId(block.id, notion);
+      return text + "\n  " + childrenText.split("\n").join("\n  ");
+    })
+  );
+
+  return content.join("\n");
+};
+
+const getTextContentOfBlockWithoutChildren = (block: BlockObjectResponse) => {
+  switch (block.type) {
+    case "paragraph":
+      return block.paragraph.rich_text.map((text) => text.plain_text).join("");
+    case "heading_1":
+      return (
+        "# " + block.heading_1.rich_text.map((text) => text.plain_text).join("")
+      );
+    case "heading_2":
+      return (
+        "## " +
+        block.heading_2.rich_text.map((text) => text.plain_text).join("")
+      );
+    case "heading_3":
+      return (
+        "### " +
+        block.heading_3.rich_text.map((text) => text.plain_text).join("")
+      );
+    case "bulleted_list_item":
+      return block.bulleted_list_item.rich_text
+        .map((text) => "- " + text.plain_text)
+        .join("");
+    case "numbered_list_item":
+      return block.numbered_list_item.rich_text
+        .map((text, i) => `${i + 1}. ${text.plain_text}`)
+        .join("");
+    case "to_do":
+      return block.to_do.rich_text
+        .map((text) => "- [ ] " + text.plain_text)
+        .join("");
+    case "toggle":
+      return block.toggle.rich_text.map((text) => "- " + text.plain_text);
+    case "child_page":
+      return `[${block.child_page.title}]()`;
+    default:
+      return "<unknown>";
+  }
+};
--- node-scripts-poc/src/main.ts
+++ node-scripts-poc/src/main.ts
@@ -1,1 +1,32 @@
-console.log("Hello");
+import { Client } from "@notionhq/client";
+import * as dotenv from "dotenv";
+import { getTextContentOfBlockFromId } from "./getTextContentOfBlock";
+dotenv.config();
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+const main = async () => {
+  const page = await notion.pages.retrieve({
+    page_id: "2c5d22f5456a44fd820db3d278ffeece",
+  });
+
+  if (!("properties" in page)) return;
+
+  console.log(
+    "Title : ",
+    page.properties.title.type === "title"
+      ? page.properties.title.title.map((block) => block.plain_text).join("")
+      : ""
+  );
+
+  console.log(
+    await getTextContentOfBlockFromId(
+      "2c5d22f5456a44fd820db3d278ffeece",
+      notion
+    )
+  );
+};
+
+main();
