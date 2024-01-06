8cdaf8a0d29cc7da74f5350d659630015f571b60
mbezard <matthieube@bam.tech>
2024-01-06 17:19:15 +0100

    feat: working example of database.query

--- node-scripts-poc/src/mainNotion.ts
+++ node-scripts-poc/src/mainNotion.ts
@@ -1,4 +1,4 @@
-import { Client } from "@notionhq/client";
+import { Client, isFullPage } from "@notionhq/client";
 import * as dotenv from "dotenv";
 import { getTextContentOfBlockFromId } from "./getTextContentOfBlock";
 dotenv.config();
@@ -7,7 +7,7 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-const main = async () => {
+export const readTitle = async () => {
   const page = await notion.pages.retrieve({
     page_id: "2c5d22f5456a44fd820db3d278ffeece",
   });
@@ -20,12 +20,44 @@ const main = async () => {
       ? page.properties.title.title.map((block) => block.plain_text).join("")
       : ""
   );
-
+};
+export const readPage = async () => {
   console.log(
     await getTextContentOfBlockFromId(
       "2c5d22f5456a44fd820db3d278ffeece",
       notion
     )
+  );
+};
+
+// const propertiesInDailyBoard = {
+//   Status: ["Backlog", "CA OK", "AT OK", "In progress", "Done"],
+//   Name: ""
+// };
+
+const main = async () => {
+  const { results } = await notion.databases.query({
+    database_id: "b12b3beb97c94b73b7a519dfd46f17d2",
+    // filter: {
+    //   property: "Status",
+    //   status: {
+    //     equals: "In progress",
+    //   },
+    // },
+  });
+
+  Promise.all(
+    results.map(async (page): Promise<void> => {
+      if (!isFullPage(page)) return;
+      if (page.properties.Name.type !== "title") return;
+
+      const content = await getTextContentOfBlockFromId(page.id, notion);
+      console.log(
+        "PAGE : ",
+        page.properties.Name.title.map((block) => block.plain_text).join("")
+      );
+      console.log("CONTENT : \n", content, "\n --- \n");
+    })
   );
 };
 
