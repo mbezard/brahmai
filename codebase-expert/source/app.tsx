733de403962ee39863f4503c1bd4e8f9c1dd1645
mbezard <matthieube@bam.tech>
2024-02-24 16:56:08 +0100

    first execsync working

--- codebase-expert/source/app.tsx
+++ codebase-expert/source/app.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Text} from 'ink';
+import {execSync} from 'child_process';
 
 type Props = {
 	name: string | undefined;
 };
 
 export default function App({name = 'Stranger'}: Props) {
+	const gitBranch = execSync('git branch --show-current').toString().trim();
+
 	return (
 		<Text>
 			Hello, <Text color="green">{name}</Text>
+			<Text>! You are on branch </Text>
+			<Text color="yellow">{gitBranch}</Text>
 		</Text>
 	);
 }
