edbfe329db25cdeb7c9483ae001f54c349c160bc
mbezard <matthieube@bam.tech>
2024-02-24 16:45:17 +0100

    init ink app

--- codebase-expert/src/app/app.ts
+++
@@ -1,9 +0,0 @@
-type Params = {
-  input: string[];
-  flags: Record<string, unknown>;
-};
-export const app = ({ input, flags }: Params) => {
-  console.log("Hello, World!");
-  console.log("Input:", input);
-  console.log("Flags:", flags);
-};
