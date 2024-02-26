b6d62df47324d2ebadad7bc715897d18c7de713c
mbezard <matthieube@bam.tech>
2024-02-26 01:06:15 +0100

    make sure to trigger questions only once each

--- codebase-expert/source/useQuestion.ts
+++ codebase-expert/source/useQuestion.ts
@@ -1,10 +1,11 @@
 import {useMutation} from '@tanstack/react-query';
 import {askQuestion} from './openai/askQuestion.js';
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import OpenAI from 'openai';
 import {Question as QuestionType} from './questions.js';
 
 export const useQuestion = (openai: OpenAI, question: QuestionType) => {
+	const hasBeenAskedRef = useRef(false);
 	const {isPending: isLoading, mutateAsync} = useMutation({
 		mutationKey: ['macro-architecture'],
 		mutationFn: () => askQuestion(openai, question.questionWithFunction),
@@ -15,7 +16,10 @@ export const useQuestion = (openai: OpenAI, question: QuestionType) => {
 	});
 
 	useEffect(() => {
-		void mutateAsync();
+		if (!hasBeenAskedRef.current) {
+			hasBeenAskedRef.current = true;
+			mutateAsync();
+		}
 	}, [mutateAsync]);
 
 	return {isLoading};
