839a7d5347022d1f66f852c683ef7a89f3e8abd8
mbezard <matthieube@bam.tech>
2024-03-03 16:27:55 +0100

    extract gettingAndWritingFilesFromFunctionOutput

--- codebase-expert/source/results/useSaveResults.ts
+++ codebase-expert/source/results/useSaveResults.ts
@@ -1,6 +1,7 @@
 import {useGlobalState} from '../globalState.js';
 import fs from 'fs';
 import {basePrompt, basePromptEndPart} from '../modules/openai/prompts.js';
+import {gettingAndWritingFilesFromFunctionOutput} from './gettingAndWritingFilesFromFunctionOutput.js';
 
 export const useSaveResults = () => {
 	const projectName = process.cwd().split('/').pop();
@@ -70,34 +71,4 @@ ${basePromptEndPart}
 		'examples',
 		'screenImplementations',
 	);
-};
-
-const gettingAndWritingFilesFromFunctionOutput = (
-	output: string,
-	key: string,
-	fileName: string,
-) => {
-	try {
-		const outputAsObject = JSON.parse(output);
-		const outputAsList = outputAsObject[key] as string[];
-
-		const content = outputAsList
-			.filter((example: string) => !example.includes('.png'))
-			.map((example: string) => {
-				let content = '';
-				try {
-					content = fs.readFileSync(example, 'utf-8');
-				} catch (e) {
-					// console.error(`Error reading file ${example}`, e);
-					content = '';
-				}
-				return `## ${example}\n\n\`\`\`\n${content}\n\`\`\``;
-			})
-			.join('\n\n');
-
-		fs.writeFileSync(`results/knowledgeFiles/${fileName}.md`, content);
-	} catch (e) {
-		console.error(`Error ${fileName}`, e);
-		fs.writeFileSync(`results/knowledgeFiles/${fileName}.md`, output);
-	}
 };
